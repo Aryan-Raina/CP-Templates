4bfa5069fd680456e1bf3fbd843a623ce7b283e7
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2020-12-23 23:55:29 +0530

    Create UFDS.cpp

---
+++ UFDS.cpp
@@ -0,0 +1,20 @@
+struct UFDS {
+    int n;
+    vector<int> len, par;
+    void init(int x) {
+        n = x;
+        par.resize(n+2);
+        len.assign(n+2, 1);
+        iota(par.begin(), par.end(), 0);
+    }
+    int fin(int v) { return par[v] == v ? v : par[v] = fin(par[v]); }
+    bool join(int a, int b) {
+        a = fin(a); b = fin(b);
+        if(a == b) return false;
+        if(len[a] < len[b])
+            swap(a, b);
+        par[b] = a;
+        len[a] += len[b];
+        return true;
+    }
+};
