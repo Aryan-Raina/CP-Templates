d73bee68b2ef9d01ad7ffe8d2c9e1251e647e5ed
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2020-12-24 18:54:14 +0530

    Create SegTree.cpp

---
+++ SegTree.cpp
@@ -0,0 +1,54 @@
+struct SegTree {
+    int N;
+    vector<int> values;
+    int NEUTRAL = 0;
+
+    int merge(int a, int b) {
+        return (a+b);
+    }
+
+    void init(int n) {
+        N = n;
+        values.resize(4*n);
+    }
+
+    void build(vector<int> &a, int x, int lx, int rx) {
+        if (rx - lx == 1) return void(values[x] = a[lx]);
+        int mid = (lx + rx)/2;
+        build(a, 2*x + 1, lx, mid);
+        build(a, 2*x + 2, mid, rx);
+        values[x] = merge(values[2*x + 1], values[2*x + 2]);
+    }
+
+    void build(vector<int> &a) {
+        build(a, 0, 0, N);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+        if (rx - lx == 1) return void(values[x] = v);
+        int mid = (lx + rx)/2;
+        if (i < mid) {
+            set(i, v, 2*x + 1, lx, mid);
+        } else {
+            set(i, v, 2*x + 2, mid, rx);
+        }
+        values[x] = merge(values[2*x + 1], values[2*x + 2]);
+    }
+
+    void set(int val, int pos) {
+        set(val, pos, 0, 0, N);
+    }
+
+    int calc(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || rx <= l) return NEUTRAL;
+        if (l <= lx && rx <= r) return values[x];
+        int mid = (lx + rx)/2;
+        int s1 = calc(l, r, 2*x + 1, lx, mid);
+        int s2 = calc(l, r, 2*x + 2, mid, rx);
+        return merge(s1, s2);
+    }
+
+    int calc(int l, int r) {
+        return calc(l, r, 0, 0, N);
+    }
+};
