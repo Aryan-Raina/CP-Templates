f0a345c251742fd46539e29e40477c69d9452594
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2021-03-20 17:05:30 +0530

    Update KosarajuSCC.cpp

--- KosarajuSCC.cpp
+++ KosarajuSCC.cpp
@@ -5,25 +5,27 @@ struct KosarajuSCC {
 
     void init(int n) { 
         N = n; adj.resize(N), radj.resize(N);
-        comp = vector<int>(N, -1), vis.resize(N); 
+        comp.assign(N, -1), vis.resize(N); 
     }
 
-    void ae(int a, int b) { adj[a].push_back(b); radj[b].push_back(a); }
+    void add_edge(int a, int b) { adj[a].push_back(b); radj[b].push_back(a); }
 
-    int dfs1(int u) {
+    void dfs1(int u) {
         vis[u] = 1;
-        for (int v : adj[u]) if (!vis[u]) dfs1(v);
+        for (int v : adj[u]) if (!vis[v]) dfs1(v);
         todo.push_back(u);
     }
 
     void dfs2(int u, int num) {
         comp[u] = num;
-        for (int v : radj[u]) if (comp[u] == -1) dfs(v, num);
+        for (int v : radj[u]) if (comp[v] == -1) dfs2(v, num);
     }
 
     void gen() {
-        for (int i = 0; i < n; i++) if (!vis[i]) dfs1(i);
+        for (int i = 0; i < N; i++) if (!vis[i]) dfs1(i);
         reverse(todo.begin(), todo.end());
-        for (int i : todo) if (comp[i] == -1) dfs2(i), comps.push_back(i);
+        for (int i : todo) if (comp[i] == -1) {
+            dfs2(i, i); comps.push_back(i);
+        }
     }
-};
+}; 
