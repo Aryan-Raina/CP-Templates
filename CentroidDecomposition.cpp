5b65de9b99569bc8f982ed451d61abd6494b6176
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2021-03-29 16:14:52 +0530

    Update CentroidDecomposition.cpp

--- CentroidDecomposition.cpp
+++ CentroidDecomposition.cpp
@@ -1,32 +1,33 @@
-void OneCentroid(int rt, vector<vector<int>> &g, vector<int> &dead) {
+int OneCentroid(int rt, vector<vector<ar<int,2>>> &g, vector<bool> &dead) {
 	int n = g.size();
 	vector<int> sz(n);
 	function<void(int,int)> get_sz = [&](int u, int pu) {
-		sz[v] = 1;
-		for (int v : g[u]) if (v != pu && !dead[v]) {
-			get_sz(v);
-			sz[u] += sz[v];
+		sz[u] = 1;
+		for (auto v : g[u]) if (v[1] != pu && !dead[v[1]]) {
+			get_sz(v[1], u);
+			sz[u] += sz[v[1]];
 		}
 	};
 	get_sz(rt, rt);
 	function<int(int,int)> dfs = [&](int u, int pu) {
-		for (int v : g[u]) if (v != pu && !dead[v]) {
-			if (sz[v] > sz[rt]/2) return dfs(v, u);
+		for (auto v : g[u]) if (v[1] != pu && !dead[v[1]]) {
+			if (sz[v[1]] > sz[rt]/2) return dfs(v[1], u);
 		}
 		return u;
 	};
 	return dfs(rt, rt);
 }
 
-int CentroidDecomposition(vector<vector<int>> &g) {
+int CentroidDecomposition(vector<vector<ar<int,2>>> &g, int k) {
 	int n = g.size();
 	vector<bool> dead(n, false);
 	int ans = 0;
-	function<int(int,int)> decompose = [&](int start) {
+	function<void(int)> decompose = [&](int start) {
 		int cen = OneCentroid(start, g, dead);
 		dead[cen] = true;
 		// do stuff with subtree of centroid here
-		for (int v : g[u]) if (!dead[v]) decompose(v);
+
+		for (auto v : g[cen]) if (!dead[v[1]]) decompose(v[1]);
 		dead[cen] = false;
 	};
 	decompose(0);
