78f5f9ca3eefc30daf976a3d753cde43d9e9f16e
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2021-04-12 13:02:51 +0530

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,12 +1,12 @@
 struct FenwickTree {
     vector<int> s; // s[i] represents sum from [i with last 1 changed to 0, i]
-    FenwickTree(int n) : s(n+5) {}
+    FenwickTree(int _n) : s(_n) {}
     void modify(int pos, int dif) { // a[pos] += dif
-        for (; pos >= 0; pos |= pos+1) s[pos] += dif; // keep changing last 0 to 1
+        for (; pos < s.size(); pos |= pos+1) s[pos] += dif; // keep changing last 0 to 1
     } 
     int calc(int pos) { // sum of values in [0, pos)
         int res = 0;
-        for (; pos < n; pos &= pos-1) res += s[pos-1]; // keep changing last 1 to 0
+        for (; pos > 0; pos &= pos-1) res += s[pos-1]; // keep changing last 1 to 0
         return res;
     }
-}
+};
