1fc166ab6d30e3f287cb3152598e5e9e424b5799
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2021-05-14 16:35:29 +0530

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,14 +1,14 @@
 template<class T> struct SegmentTree {
    int n; vector<T> values; 
-   T NEUTRAL_ELEMENT = 0;
-   SegmentTree(int n) : n(n), values(2*n, NEUTRAL_ELEMENT) {}
+   T IDENTITY = 0;
+   SegmentTree(int n, int id) : n(n), values(2*n, IDENTITY = id) {}
    T calc_op(T a, T b) { return a + b; }
    void modify(int i, T v) {
       for (values[i += n] = v; i >>= 1; ) 
          values[i] = calc_op(values[2*i], values[2*i + 1]);
    }
    T calc(int l, int r) {
-      T rans = NEUTRAL_ELEMENT, lans = NEUTRAL_ELEMENT;
+      T rans = IDENTITY, lans = IDENTITY;
       for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
          if (l & 1) lans = calc_op(lans, values[l++]);
          if (r & 1) rans = calc_op(values[--r], rans);
