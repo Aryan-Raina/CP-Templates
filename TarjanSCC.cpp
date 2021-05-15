bd9f8845a0c008d1ba41e8b7f06a62612e604f06
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2021-05-15 17:17:30 +0530

    Update TarjanSCC.cpp

--- TarjanSCC.cpp
+++ TarjanSCC.cpp
@@ -1,26 +1,23 @@
 struct TarjanSCC {
-    int N, timer = 0; vector<vector<int>> adj;
-    vector<int> disc, comp, st, comps;
+   int N, timer = 0; vector<vector<int>> adj;
+   vector<int> disc, st, comp;
 
-    void init(int n) { 
-        N = n; adj.resize(N), disc.resize(N);
-        comp = vector<int>(N, -1); 
-    }
+   TarjanSCC(int n) : N(n), disc(N, 0), comp(N, -1), adj(N) {}
 
-    void ae(int a, int b) { adj[a].push_back(b); }
+   void add_edge(int a, int b) { adj[a].push_back(b); }
 
-    int dfsSCC(int u) {
-        int low = disc[u] = ++timer; st.push_back(u);
-        for (int v : adj[u]) if (comp[v] != -1) low = min(low, disc[v]?:dfs(v); )
-        if (low == disc[u]) {
-            comps.push_back(u);
-            for (int v = -1; v != u; ) comp[v = st.back()] = x, st.pop_back;
-        }
-        return low;
-    }
+   int dfsSCC(int u) {
+      int low = disc[u] = ++timer; st.push_back(u);
+      for (int v : adj[u]) if (comp[v] == -1) 
+         low = min(low, disc[v] ? : dfsSCC(v));
 
-    void gen() {
-        for (int i = 0; i < n; i++) if (!disc[i]) dfsSCC(i);
-        reverse(comps.begin(), comps.end());
-    }
+      if (low == disc[u]) {
+         for (int v = -1; v != u; ) 
+            comp[v = st.back()] = u, st.pop_back();
+      }
+
+      return low;
+   }
+
+   void gen() { for (int i = 0; i < N; i++) if (!disc[i]) dfsSCC(i); }
 };
