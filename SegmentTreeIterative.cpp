bd58ede86a1d8df85d348ebd504c0a0f6692edf3
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2025-01-21 20:25:26 +0530

    Update SegmentTreeIterative.cpp

--- SegmentTreeIterative.cpp
+++ SegmentTreeIterative.cpp
@@ -1,19 +1,21 @@
+// kactl 
 
-template<class T> struct SegmentTree {
-    int n; vector<T> values; 
-    T IDENTITY = 0;
-    SegmentTree(int n, int id) : n(n), values(2*n, IDENTITY = id) {}
-    T calc_op(T a, T b) { return a + b; }
-    void modify(int i, T v) {
-       for (values[i += n] = v; i >>= 1; ) 
-          values[i] = calc_op(values[2*i], values[2*i + 1]);
-    }
-    T calc(int l, int r) {
-       T rans = IDENTITY, lans = IDENTITY;
-       for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-          if (l & 1) lans = calc_op(lans, values[l++]);
-          if (r & 1) rans = calc_op(values[--r], rans);
-       }
-       return calc_op(lans, rans);
-    }
+struct Tree {
+	typedef int T;
+	static constexpr T unit = INT_MIN;
+	T f(T a, T b) { return max(a, b); } // (any associative fn)
+	vector<T> s; int n;
+	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
+	void update(int pos, T val) {
+		for (s[pos += n] = val; pos /= 2;)
+			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+	}
+	T query(int b, int e) { // query [b, e)
+		T ra = unit, rb = unit;
+		for (b += n, e += n; b < e; b /= 2, e /= 2) {
+			if (b % 2) ra = f(ra, s[b++]);
+			if (e % 2) rb = f(s[--e], rb);
+		}
+		return f(ra, rb);
+	}
 };
